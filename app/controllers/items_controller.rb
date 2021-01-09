62b040bcf3eeb9aebd4807ad098bf10d9cc6d667
yux521 <73996546+yux521@users.noreply.github.com>
2021-01-09 15:09:58 +0900

    アクセス制限の修正
    検証ツールからボタンを表示させる、URLの直接入力をするといった不正なアクセスを防ぐため

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update]
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :update]
 
   def index
     @items = Item.all.order("created_at DESC")
