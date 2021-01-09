7ff5a96e2bae301fa84391cad0128332684226da
yux521 <73996546+yux521@users.noreply.github.com>
2021-01-09 12:38:52 +0900

    編集頁更新後の分岐処理実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,6 +27,15 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render action: :edit
+    end
+  end
+
   private
 
   def item_params
